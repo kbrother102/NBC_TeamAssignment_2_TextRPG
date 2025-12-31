a146a2242aa4eb87464738b8cd5cf10809996dbb
Daype1015 <mj1527123@gmail.com>
2025-12-31 10:30:16 +0900

    mjp monster
    monster commit

--- NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Monster.h
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Monster.h
@@ -1,5 +1,16 @@
 ﻿#pragma once
+#include <iostream>
+#include <random>
+#include <string>
+
 class Monster
 {
+public:
+	virtual std::string GetName() = 0;
+	virtual int GetHealth() = 0;
+	virtual int GetAttack() = 0;
+	virtual void TakeDamaged(int damage) = 0;
+	virtual void Buff() = 0;
+	virtual void Attack() = 0;
 };
 
