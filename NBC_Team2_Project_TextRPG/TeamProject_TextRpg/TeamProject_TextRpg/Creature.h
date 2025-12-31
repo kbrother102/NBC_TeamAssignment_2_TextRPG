255852490cdd102286252b947960958b309a6293
kbrother102 <kbrother102@gmail.com>
2025-12-31 20:05:37 +0900

    ✨ update
    크리쳐에 캐릭터와 몬스터의 가상함수를 만들었다

--- NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Creature.h
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Creature.h
@@ -8,5 +8,7 @@ public:
     virtual ~Creature() {};
     //타입: Monster, Player (일단기본)
     virtual std::string GetType() = 0 ;
+    virtual std::string GetName() = 0;
+    virtual void TakeDamage(int dmg) = 0;
 };
 
