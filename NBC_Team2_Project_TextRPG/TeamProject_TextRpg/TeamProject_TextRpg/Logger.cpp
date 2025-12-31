cda8aa0336eef0e6d0bec29cb3dc7e66fac9ecdc
saltlake00 <saltlake5470@gmail.com>
2025-12-31 16:43:03 +0900

    로거 ui 색상 추가, 스텟매니저 get set 추가

--- NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Logger.cpp
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Logger.cpp
@@ -1,46 +1,72 @@
 ﻿#include "Logger.h"
 #include <iostream>
 
-std::vector<std::string> Logger::logs;
-
 // 색상 코드
 #define COLOR_RESET   "\033[0m"
 #define COLOR_RED     "\033[31m"
 #define COLOR_YELLOW  "\033[33m"
 #define COLOR_CYAN    "\033[36m"
 
-// ★ 여기도 반드시 'Logger::Add' 여야 합니다! ★
-void Logger::Add(LogType type, const std::string& msg)
-{
-    std::string prefix;
-    std::string colorCode;
+// 자주 쓰는 것들 선언 (여기서 선언했으니, 아래에서는 std:: 빼고 씁니다)
+using std::cout;
+using std::endl;
+using std::string;
+using std::to_string;
+using std::vector; // vector도 추가했습니다.
 
-    switch (type)
+// static 변수 메모리 할당
+vector<string> Logger::Logs;
+
+void Logger::Add(LogType type, const string& msg)
+{
+    string Prefix = ""; // 기본값 빈 문자열
+    string ColorCode = COLOR_RESET;
+
+    switch (type) // 태그에 따른 로그 말머리 및 컬러지정
     {
-    case INFO:   prefix = "[정보]"; colorCode = COLOR_CYAN;   break;
-    case COMBAT: prefix = "[전투]"; colorCode = COLOR_RED;    break;
-    case ERROR:  prefix = "[오류]"; colorCode = COLOR_YELLOW; break;
+    case INFO:
+        Prefix = "[정보] "; 
+        ColorCode = COLOR_CYAN;
+        break;
+
+    case COMBAT:
+        Prefix = "[전투] ";
+        ColorCode = COLOR_RED;
+        break;
+
+    case ERROR:
+        Prefix = "[오류] ";
+        ColorCode = COLOR_YELLOW;
+        break;
+
+    case UI:
+        Prefix = ""; // UI 말머리 없음
+        ColorCode = COLOR_RESET; // 혹은 원하는 색 (예: 흰색)
+        break;
     }
 
-    std::string finalLog = prefix + " " + msg;
+    // prefix가 비어있으면 그냥 msg만 나감
+    string FinalLog = Prefix + msg;
 
-    // 1. 콘솔 출력
-    std::cout << colorCode << finalLog << COLOR_RESET << std::endl;
+    // 1. 콘솔 출력 
+    cout << ColorCode << FinalLog << COLOR_RESET << endl;
 
     // 2. 벡터 저장
-    logs.push_back(finalLog);
-    if (logs.size() > MAX_LOG_COUNT)
+    Logs.push_back(FinalLog);
+    if (Logs.size() > MAX_LOG_COUNT)
     {
-        logs.erase(logs.begin());
+        Logs.erase(Logs.begin());
     }
 }
 
-const std::vector<std::string>& Logger::GetLogs()
+// 추후 UI 변경시를 위한 벡터 문자열 저장
+
+const vector<string>& Logger::GetLogs()
 {
-    return logs;
+    return Logs;
 }
 
 void Logger::Clear()
 {
-    logs.clear();
+    Logs.clear();
 }
