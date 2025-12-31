b790403813e1ab98bd17494af89e807d9f7b9966
kbrother102 <kbrother102@gmail.com>
2025-12-31 16:40:58 +0900

    ✨ update
    랜덤클래스, 보상클래스 생성

--- NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Creature.h
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Creature.h
@@ -1,5 +1,12 @@
 ﻿#pragma once
+#include <string>
 class Creature
 {
+protected:
+    std::string Type_;
+public:
+    virtual ~Creature() {};
+    //타입: Monster, Player (일단기본)
+    virtual std::string GetType() = 0 ;
 };
 
---
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/RewardManager.cpp
@@ -0,0 +1,42 @@
+﻿#include "RewardManager.h"
+#include "Random.h"
+#include "Item.h"
+
+void RewardManager::ProcessReward(Creature* Monster, Creature* Player)
+{
+    //몬스터 스탯 컴포넌트에서 경험치, 돈을 가져옴
+    //GetExp() 는 몬스터의 경험치를 출력해주는 함수, component의 getexp()를 출력
+    // int MonsterExp = monster->GetExp();
+    // int MOnsterGold = monster->GetGold();
+    
+     
+    //플레이어에게 추가
+    //set함수라면
+    //Player->SetExp(Player.GetExp() + MonsterExp);
+    //add함수라면
+    //Player->AddExp(MonsterExp);
+    
+    //set함수라면
+    //Player->SetGold(Player.GetGold() + MonsterGold);
+    //add함수라면
+    //Player->AddGold(MonsterGold);
+
+}
+
+RewardManager& RewardManager::GetInstance()
+{
+    static RewardManager instance;
+    return instance;
+}
+//사용법 RewardManager::GetInstance().ProcessReward(매개변수)
+//RewardManager& RM = RewardManager::GetInstance() ; 으로 짧게 이름 바꿀수도잇음
+
+bool RewardManager::isItemDrop()
+{
+    if (Random::GetRandFloat() < 0.3)
+    {
+        return true;
+    }
+ 
+    return false;
+}
