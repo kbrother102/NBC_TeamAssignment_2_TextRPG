b57b8a36d01ce1933e4440ee45567bf17c05a10c
titanaw <wjdghtmd1907@naver.com>
2025-12-31 11:12:19 +0900

    action
    randomuse아이템 수정
    void 몇개 추가

--- NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Action.h
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Action.h
@@ -2,19 +2,15 @@
 class Action
 {
 	
-	void Attack(); 
+	void Attack();
 	
 
-	void UseItem(Item* Item, Character* character);
+	void RandUseItem(Item* Item, Inventory* inventory);
 	
-
+	void AddItem(Item* item);
+	void TakeDamage(int dmg);
 
 private:
-	//TODO : 멤버변수 뒤에는 언더바
-	class Character* character = nullptr;
-	Monster* monster = nullptr;
-	Inventory* inventory = nullptr;
-	//TODO : 액션은 캐릭터와 몬스터를 사용하지 않는다(매개변수로 받음).  멤버변수 필요없음
-	//TODO : 이 액션컴포넌트를 가지는 주인 등록하기 (생성시에 받음)
+	Creature* owner_ = nullptr;
 };
 
