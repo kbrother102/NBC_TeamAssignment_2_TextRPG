142ddfd594815ddeeda5d7542aed185caca3bbe8
Daype1015 <mj1527123@gmail.com>
2026-01-02 21:02:52 +0900

    퇴근하자
    야미~~

--- NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Mob1.h
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Mob1.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include "Monster.h"
+#include "Action.h"
 #include "StatComponent.h"
 
 
@@ -9,39 +10,41 @@ public:
 
 	ChangMinKong()
 	{
-		monasterStat_ = new StatComponent();
+		monStat_ = new StatComponent();
+		monAct_ = new Action();
 	}
 
 	//몬스터 고유 함수
 	void SpawnMob(int level);
 	void TakeDamage(int dmg);
-	StatComponent* GetStatComponent() { return monasterStat_; }
+	StatComponent* GetStatComponent() { return monStat_; }
+	Action* GetActionComponent() { return monAct_; }
 	void UseSkill();
 
 	//게터함수
 	//string
-	std::string GetType() { return monasterStat_->GetType(); }
+	std::string GetType() { return monStat_->GetType(); }
 	std::string GetName() const { return name_; }
 
 	//int
-	int GetHealth() { return monasterStat_->GetHp(); }
-	int GetAttack() { return monasterStat_->GetAttack(); }
-	int GetExp() { return monasterStat_->GetExp(); }
-	int GetGold() { return monasterStat_->GetGold(); }
+	int GetHealth() { return monStat_->GetHp(); }
+	int GetAttack() { return monStat_->GetAttack(); }
+	int GetExp() { return monStat_->GetExp(); }
+	int GetGold() { return monStat_->GetGold(); }
 
 	//bool
-	bool GetAlive() { return monasterStat_->GetIsDead(); }
+	bool GetIsDead() { return monStat_->GetIsDead(); }
 	bool GetIsBoss() { return bIsBoss_; }
 
 	~ChangMinKong()
 	{
 		//소멸자
-		delete monasterStat_;
+		delete monStat_;
+		delete monAct_;
 	}
 
 private:
 	//TODO : 공통된 변수 몬스터(Monster.h)에 넣기
-	class StatComponent* monasterStat_;
 	std::string name_ = "mob1";
 	bool bIsBoss_ = false;
 };
