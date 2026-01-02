a5f169ac81f39f962baa8be83cfc33a5af496672
Daype1015 <mj1527123@gmail.com>
2026-01-02 10:16:11 +0900

    🎉 add
    추가 몬스터 생성

---
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Mob2.cpp
@@ -0,0 +1,45 @@
+﻿#include "Mob2.h"
+#include "Random.h"
+
+void BongJaeKong::SpawnMob(int level)
+{
+	// 몬스터가 살아있는지 체크
+	if (bIsAlive_ == false)
+	{
+		if (level > 9 && bIsBoss_ == false)
+		{
+			//보스 몬스터 모자를 벗은 봉재튜터님
+			health_ = (level * Random::GetRandInt(30, 45));
+			attack_ = (level * Random::GetRandInt(8, 15));
+			bIsAlive_ = true;
+			bIsBoss_ = true;
+		}
+		else
+		{
+			//일반몬스터 일반 봉재튜터님
+			//전달받은 캐릭터의 레벨에따라 범위안에 능력치 랜덤부여
+			health_ = (level * Random::GetRandInt(20, 30));
+			attack_ = (level * Random::GetRandInt(5, 10));
+			bIsAlive_ = true;
+			bIsBoss_ = false;
+		}
+	}
+	else return;
+}
+
+void BongJaeKong::DieMob()
+{
+
+	if (bIsAlive_ == true)
+	{
+		bIsAlive_ = false;
+		//몬스터처치 메세지 출력
+	}
+	else return;
+}
+
+void BongJaeKong::TakeDamage(int dmg)
+{
+	health_ -= dmg;
+	if (health_ <= 0);
+}
---
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Mob2.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+#include "Monster.h"
+class BongJaeKong : public Monster
+{
+public:
+
+	BongJaeKong() {}
+
+	void SpawnMob(int level);
+	void DieMob();
+	void TakeDamage(int dmg);
+
+	//게터함수
+	std::string GetType() { return Type_; }
+	std::string GetName() { return name_; }
+	int GetHealth() { return health_; }
+	int GetAttack() { return attack_; }
+	int GetExp() { return exp_; }
+	int GetGold() { return gold_; }
+	bool GetAlive() { return bIsAlive_; }
+	bool GetIsBoss() { return bIsBoss_; }
+
+	~BongJaeKong()
+	{
+		//소멸자
+	}
+private:
+	std::string name_ = "mob2";
+	int health_ = 0;
+	int attack_ = 0;
+	int exp_ = 0;
+	int gold_ = 0;
+	bool bIsAlive_ = false;
+	bool bIsBoss_ = false;
+};
+
