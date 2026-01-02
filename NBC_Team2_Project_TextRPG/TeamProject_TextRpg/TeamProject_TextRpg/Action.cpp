854bbdb04681132c0f0691c47f15adc1d817db1d
titanaw <wjdghtmd1907@naver.com>
2026-01-02 12:17:22 +0900

    액션
    로거

--- NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Action.cpp
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Action.cpp
@@ -15,60 +15,77 @@
 void Action::Attack(Creature* target)
 {
     //없으면 종료
-    if(owner_ == nullptr)
+    
+    if(owner_ == nullptr || target == nullptr)
   {
       return;
   }
-   
-     
-	//타겟의 체력에서 공격자의 공격력만큼 차감
-	target->TakeDamage();
+	//타겟의 체력에서 공격자의 공격력만큼 차감//여기 대미지 계산확실하게
+	target->TakeDamage(owner_->GetStatComponent()->GetAttack());
     
-    
-	
-
-   
     //로그 공격했다 구현
     Logger::Add(LogType::COMBAT, owner_->GetName()+ "가"+ target->GetName()+ "을(를) 공격했다!");
-    Logger::Add(LogType::COMBAT, "데미지 : " + owner_->GetDamage());
+    Logger::Add(LogType::COMBAT, "데미지 : " + owner_->GetStatComponent()->GetAttack());
     
 }
 
 
 //아이템 획득.
-void Action::RandUseItem(Item* item, Inventory* inventory)
+void Action::RandUseItem(Inventory* inventory)
 {
     int r = Random::GetRandInt(0, 99);
-	
+
     //인벤토리에 없고 인벤 사이즈가 0이면 종료
-    if (!inventory || inventory->GetPoint() == 0)
+    if (!inventory || !owner_)
     {
         return;
     }
-    
+
+	//TODO: 아이템 개수 체크
+    //  if (inventory->GetItemCount() == 0)   return;
     //30% 확률로 아이템 사용
     if (r < 30)
     {
 
-   //인벤토리에서 랜덤으로 아이템 선택 , 0, inventory->GetSize()-1);
-    int random = Random::GetRandInt(0, inventory->GetPoint());
-   //item = inventory->UseItem(random,stats); 
-  
-  
+        //인벤토리에서 랜덤으로 아이템 선택 , 0, inventory->GetSize()-1);
+       //TODO :int random = Random::GetRandInt(0, inventory.());
+        //아이템을 랜덤으로 선택한것을 저장.
+       /*TODO Item* item = inventory->GetItemAt(random);
+
+        if(!item)
+        {
+            return;
+		}
  
-   //로그 아이템을 사용했다 구현
-   //Log();
-    if(item->GetName() == "HpPotion")
-    {
-      std::cout << "체력이 회복되었다!" << std::endl;
+        inventory->UseItem(random, *owner_->GetStatComponent());*/
+
+
+
+        /*로그 아이템을 사용했다 구현
+        if (item->GetName() == "HpPotion")
+        {
+            std::cout << "체력이 회복되었다!" << std::endl;
+        }
+        if (item->GetName() == "AttackPotion")
+        {
+            std::cout << "공격력이 상승했다!" << std::endl;
+        }
+        Logger::Add(LogType::INFO, owner_->GetName() + "가 " + item->GetName() + "을(를) 사용했다!");
+        */
+
     }
-    if(item->GetName() == "AttackPotion")
+}
+
+void Action::Die()
+{
+    //없으면 종료
+    if(owner_ == nullptr)
     {
-      std::cout << "공격력이 상승했다!" << std::endl;
+        return;
     }
-	Logger::Add(LogType::INFO, owner_->GetName() + "가 " + item->GetName() + "을(를) 사용했다!");
-    
-   
+
+	//로그 사망했다 구현
+    Logger::Add(LogType::INFO, owner_->GetName() + "이(가) 사망했다!");
 }
 
 
--- NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Action.h
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Action.h
@@ -10,8 +10,9 @@ public:
 	//공격.
 	void Attack(Creature* target);
 	//아이템 랜덤 사용.
-	void RandUseItem(Item* Item, Inventory* inventory);
+	void RandUseItem(Inventory* inventory);
 
+	void Die();
 	//데미지 받기.
 	//void TakeDamage(int dmg);
 
