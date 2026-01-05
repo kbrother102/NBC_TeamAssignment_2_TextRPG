40db3e2fc7c5567a34f3737f9133e5707d338215
csh8696-git <csh8696@gmail.com>
2026-01-05 19:11:16 +0900

    함수 추가 및 일부 수정
    생성자 간략화
    TakeDamage override 포함
    필요 없는 소멸자 제거
    가시성 소폭 추가

--- NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Character.h
+++ NBC_Team2_Project_TextRPG/TeamProject_TextRpg/TeamProject_TextRpg/Character.h
@@ -8,22 +8,24 @@ class StatComponent;
 class Action;
 
 class Character : public Creature
-{
+{	//스태틱 따로 분류
+public:
+	//Create를 통해서만 생성하게 변경
+	static std::unique_ptr<Character> Create(const std::string& name);
+	//캐릭터 명 부적합 판정
+	static bool IsValidName(const std::string& name);
+
 public:
 	//호출 시 캐릭터 이름 변경 및 인스턴스 생성
 	//Character(const std::string& name);
 	// 크리쳐 타입 추가
 	std::string GetType() override;
-	//Create를 통해서만 생성하게 변경
-	static std::unique_ptr<Character> Create(const std::string& name);
-	//캐릭터 명 부적합 판정
-	static bool IsValidName(const std::string& name);
-	//포인터 소멸자
-	~Character() = default;
+	//생성자 스텟 묶음 분리
+	void StartCharacterStat(int Level);
 	//호출 시 경험치 획득(전투 승리 시)
 	//void GainExp(int amount);
 	//피격 함수
-	void TakeDamage(int dmg);
+	void TakeDamage(int dmg) override;
 	//아이템 사용 함수
 	bool UseItem(int index); //player.UseItem(0); 으로 사용 호출 가능.
 	//골드 변경
